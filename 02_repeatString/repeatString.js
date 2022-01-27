2aad5415ade5664e2cc9fdd1fc074791f44e9fef
milanmomcilovic96 <milanmomcilovic96@gmail.com>
2022-01-27 14:10:14 +0100

    First 3 task completed!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+let word, number;
+const repeatString = function(word, number) {
+    if(number <0){
+        return 'ERROR';
+    }
+    
+    let text = '';
+    for(let i = 0; i<number; i++){
+        text += word;
+    }
+    return text;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,22 @@
-const reverseString = function() {
+let text;
+const reverseString = function(text) {
+    
+    // let reversedText = [];
+    // lengthOfText = text.length;
+    // for(let i = 1; i <= lengthOfText; i++){
+    //     reversedText[i - 1] = text.charAt(lengthOfText - i);
+    // }
+    // return reversedText.join('');
 
+    return text.split('').reverse().join('');
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+// for petlja
+// od 0 do duzine stringa 
+// poslednji karakter ubaci u niz
+// odraditi join
+
