3bf56b2de4ee55acbf763a0402352808640b76e7
milanmomcilovic96 <milanmomcilovic96@gmail.com>
2022-01-28 17:09:13 +0100

    Task #04 Completed!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(theArray, ...theArgs) {
+    for(let i = 0; i < theArgs.length; i++){
+        for(let j = 0; j < theArray.length; j++){
+            if(theArgs[i] === theArray[j]){
+                theArray.splice(j, 1)
+            }
+        }
+    }
+    
+    return theArray;
 };
 
 // Do not edit below this line
