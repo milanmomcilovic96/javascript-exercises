24dc4d05c225cca1a324a55f55ad2671f8da7955
milanmomcilovic96 <milanmomcilovic96@gmail.com>
2022-01-28 17:28:23 +0100

    Task #05 Completed!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,33 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+
+    if(typeof(start) !== 'number' || typeof(end) !== 'number'){
+        return 'ERROR';
+    }
+
+
+    if(start < end){
+        let sum = 0;
+        for(let i = start; i <= end; i++){
+            sum += i;             
+        }
+        if(sum < 0){
+            return "ERROR";
+        }else{
+            return sum;
+        }
+    }else if(start > end){
+        let sum = 0;
+        for(let i = end; i <= start; i++){
+            sum += i;
+        }
+        if(sum < 0){
+            return "ERROR";
+        }else{
+            return sum;
+        }
+    }else{
+        return end;
+    }
 
 };
 
